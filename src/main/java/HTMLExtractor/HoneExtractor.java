5ce6de5096f8b8c9b969a2f61e6458548e968797
Maria Vincent <mvincen2@vols.utk.edu>
2015-08-25 17:25:35 -0400

    cleanup

--- src/main/java/HTMLExtractor/HoneExtractor.java
+++ src/main/java/HTMLExtractor/HoneExtractor.java
@@ -1,6 +1,7 @@
 package STIXExtractor;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.UUID;
 
 import java.io.IOException;
@@ -14,7 +15,6 @@ import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.namespace.QName;					
 
 import org.mitre.stix.stix_1.STIXPackage;
-import org.mitre.stix.stix_1.STIXHeaderType;
 import org.mitre.cybox.cybox_2.Observables;
 import org.mitre.cybox.cybox_2.Observable;
 import org.mitre.cybox.cybox_2.ObservableCompositionType;
@@ -41,7 +41,12 @@ import org.mitre.cybox.objects.NetworkFlowLabelType;
 
 import org.xml.sax.SAXException;			
 
-public class HoneExtractor extends HTMLExtractor	{
+/**
+ * Hone data to STIX format extractor.
+ *
+ * @author Maria Vincent
+ */
+public class HoneExtractor extends HTMLExtractor {
 						
 	private static final Logger logger = LoggerFactory.getLogger(HoneExtractor.class);
 	private static String[] HEADERS = {"user", "uid", "proc_pid", "proc_ppid", "path", "argv", "conn_id", "timestamp_epoch_ms", 
@@ -66,10 +71,10 @@ public class HoneExtractor extends HTMLExtractor	{
 	private STIXPackage stixPackage;
 
 	//if hostname is not given
-	public HoneExtractor(String honeInfo)	{
+	public HoneExtractor(String honeInfo) {
 		stixPackage = extract(honeInfo);
 	}
-	public HoneExtractor(String honeInfo, String hostname)	{
+	public HoneExtractor(String honeInfo, String hostname) {
 		this.HOSTNAME = hostname;
 		stixPackage = extract(honeInfo);
 	}
@@ -78,20 +83,27 @@ public class HoneExtractor extends HTMLExtractor	{
 		return stixPackage;
 	}
 
-	private STIXPackage extract (String honeInfo)	{
-
+	private STIXPackage extract (String honeInfo) {
 		try	{
 			List<CSVRecord> records = getCSVRecordsList(HEADERS, honeInfo); 
-			if (records.size() == 0) return null;
-			
-			int start;
-			CSVRecord record = records.get(0);
-			if (record.get(0).equals(USER))	{
-				if (record.size() == 1) return null;
-				else start = 1;
-			}
-			else start = 0;
 
+			if (records.size() == 0) {
+				return null;
+			}
+			
+			CSVRecord record = records.get(0);
+			int start;
+			if (record.get(0).equals(USER))	{
+				if (record.size() == 1) {
+					return null;
+				} else {
+					start = 1;
+				}
+			} else {
+				start = 0;
+			}
+
+			stixPackage = initStixPackage("Hone");
 			Observables observables = initObservables();
 
 		 	for (int i = start; i < records.size(); i++)	{
@@ -109,35 +121,45 @@ public class HoneExtractor extends HTMLExtractor	{
 				Observable accountObservable = null;
 				Observable flowObservable = null;
 
-				//host
-				if (HOSTNAME != null)	
+				/* host */
+				if (HOSTNAME != null) {	
 					hostnameObservable = setHostObservable(HOSTNAME, "Hone");
+				}
 				
-				//software
-				if (!record.get(PATH).isEmpty())	{
+				/* software */
+				if (!record.get(PATH).isEmpty()) {
+
 					//processing process info
 					ProcessObjectType process = new ProcessObjectType()
 						.withName(new StringObjectPropertyType()	//path as process name
 							.withValue(record.get(PATH)));
-					if (!record.get(PROC_PID).isEmpty())
+
+					if (!record.get(PROC_PID).isEmpty()) {
 						process
 							.withPID(new UnsignedIntegerObjectPropertyType() 
 								.withValue(record.get(PROC_PID)));
-					if (!record.get(PROC_PPID).isEmpty())
+					}
+
+					if (!record.get(PROC_PPID).isEmpty()) {
 						process
 							.withParentPID(new UnsignedIntegerObjectPropertyType() 
 								.withValue(record.get(PROC_PPID)));
-					if (!record.get(ARGV).isEmpty())	{
+					}
+
+					if (!record.get(ARGV).isEmpty()) {
 						ArgumentListType argvs = new ArgumentListType();
 						String[] argvArray = record.get(ARGV).split(" ");
-						for (int j = 0; j < argvArray.length; j++)
+
+						for (int j = 0; j < argvArray.length; j++) {
 							argvs
 								.withArguments(new StringObjectPropertyType()
-									.withValue(argvArray[j]));	
+									.withValue(argvArray[j]));
+						}	
 						
-						if (!argvs.getArguments().isEmpty())
+						if (!argvs.getArguments().isEmpty()) {
 							process
 								.withArgumentList(argvs);
+						}
 					}
 
 					softwareObservable = new Observable()	
@@ -159,41 +181,96 @@ public class HoneExtractor extends HTMLExtractor	{
 										.withProperties(process))));
 				}
 
-				System.out.println("after softname");
-				//source IP
-				if (!record.get(SOURCE_IP).isEmpty())
+				/* source IP */
+				if (!record.get(SOURCE_IP).isEmpty()) {
 					observables
 						.withObservables(srcIpObservable = setIpObservable(record.get(SOURCE_IP), "Hone"));
+				}
 
-				//destination IP
-				if (!record.get(DEST_IP).isEmpty())
+				/* destination IP */
+				if (!record.get(DEST_IP).isEmpty()) {
 					observables
 						.withObservables(dstIpObservable = setIpObservable(record.get(DEST_IP), "Hone"));
+				}
 
-				//source Port
-				if (!record.get(SOURCE_PORT).isEmpty())
+				/* source Port */
+				if (!record.get(SOURCE_PORT).isEmpty()) {
 					observables
 						.withObservables(srcPortObservable = setPortObservable(record.get(SOURCE_PORT), "Hone"));
+				}
 				
-				//destination Port
-				if (!record.get(DEST_PORT).isEmpty())
+				/* destination Port */
+				if (!record.get(DEST_PORT).isEmpty()) {
 					observables
 						.withObservables(dstPortObservable = setPortObservable(record.get(DEST_PORT), "Hone"));
+				}
 
-				//source address, source address -> source IP, source address -> source Port
-				if (!record.get(SOURCE_IP).isEmpty() && !record.get(SOURCE_PORT).isEmpty())
+				/* source address -> source IP, source address -> source Port */
+				if (!record.get(SOURCE_IP).isEmpty() && !record.get(SOURCE_PORT).isEmpty()) {
 					observables
 						.withObservables(srcAddressObservable = getAddressObservable (record.get(SOURCE_IP), srcIpObservable.getId(), 
 									record.get(SOURCE_PORT), srcPortObservable.getId(), "Hone"));
+				}
 
-				//destination address, destination address -> destination IP, destination address -> destination Port
-				if (!record.get(DEST_IP).isEmpty() && !record.get(DEST_PORT).isEmpty())
+				/* destination address -> destination IP, destination address -> destination Port */
+				if (!record.get(DEST_IP).isEmpty() && !record.get(DEST_PORT).isEmpty()) {
 					observables
 						.withObservables(dstAddressObservable = getAddressObservable (record.get(DEST_IP), dstIpObservable.getId(), 
 									record.get(DEST_PORT), dstPortObservable.getId(), "Hone"));
+				}
 
-				//account
-				if (HOSTNAME != null && !record.get(UID).isEmpty())	
+				//flow, flow -> dstAddress, flow -> srdAddress
+				if (!record.get(DEST_IP).isEmpty() && !record.get(DEST_PORT).isEmpty() &&
+					!record.get(SOURCE_IP).isEmpty() && !record.get(SOURCE_PORT).isEmpty())	{	
+					flowObservable =  initFlowObservable("Hone")
+						.withObject(new ObjectType()
+							.withId(new QName("gov.ornl.stucco", "flow-" + ipToLong(record.get(SOURCE_IP)) + "_" +record.get(SOURCE_PORT) + "-" + 
+								ipToLong(record.get(DEST_IP)) + "_" + record.get(DEST_PORT), "stucco"))
+							.withDescription(new org.mitre.cybox.common_2.StructuredTextType()
+								.withValue(record.get(SOURCE_IP) + ", port " +record.get(SOURCE_PORT) + " to " + 
+									record.get(DEST_IP) + ", port " + record.get(DEST_PORT)))
+							.withProperties(new NetworkFlowObject()
+								.withNetworkFlowLabel(new NetworkFlowLabelType()
+									.withSrcSocketAddress(new SocketAddress()
+										.withObjectReference(srcAddressObservable.getId()))
+									.withDestSocketAddress(new SocketAddress()
+										.withObjectReference(dstAddressObservable.getId())))));
+
+					CustomPropertiesType properties = new CustomPropertiesType();
+					if (!record.get(TIMESTAMP_EPOCH_MS).isEmpty()) {
+						properties
+							.withProperties(new Property() 	
+								.withName("Start_Time")
+								.withValue(record.get(TIMESTAMP_EPOCH_MS)));
+					}
+
+					if (!record.get(BYTE_CNT).isEmpty()) {
+						properties
+							.withProperties(new Property() 	
+								.withName("Total_Bytes")
+								.withValue(record.get(BYTE_CNT)));
+					}
+
+					if (!record.get(PACKET_CNT).isEmpty()) {
+						properties
+							.withProperties(new Property() 	
+								.withName("Total_Packets")
+								.withValue(record.get(PACKET_CNT)));
+					}
+
+					if (!properties.getProperties().isEmpty()) {
+						flowObservable
+							.getObject()
+								.getProperties()
+									.withCustomProperties(properties);
+					}
+
+					observables
+						.withObservables(flowObservable);
+				}
+
+				/* account */
+				if (HOSTNAME != null && !record.get(UID).isEmpty()) {	
 					observables	
 						.withObservables( accountObservable = new Observable()	
 							.withId(new QName("gov.ornl.stucco", "account-" + UUID.randomUUID().toString(), "stucco"))
@@ -208,133 +285,70 @@ public class HoneExtractor extends HTMLExtractor	{
 										.withValue(record.get(UID)))
 									.withFullName(new StringObjectPropertyType()
 										.withValue(record.get(USER))))));
-
-				//flow, flow -> dstAddress, flow -> srdAddress
-				if (!record.get(DEST_IP).isEmpty() && !record.get(DEST_PORT).isEmpty() &&
-					!record.get(SOURCE_IP).isEmpty() && !record.get(SOURCE_PORT).isEmpty())	{	
-					flowObservable = new Observable()
-						.withId(new QName("gov.ornl.stucco", "flow-" + UUID.randomUUID().toString(), "stucco"))
-						.withTitle("Flow")
-						.withObservableSources(getMeasureSourceType("Hone"))
-						.withObject(new ObjectType()
-							.withId(new QName("gov.ornl.stucco", "flow-" + record.get(SOURCE_IP) + "_" +record.get(SOURCE_PORT) + "-" + 
-								record.get(DEST_IP) + "_" + record.get(DEST_PORT), "stucco"))
-							.withDescription(new org.mitre.cybox.common_2.StructuredTextType()
-								.withValue(record.get(SOURCE_IP) + ", port " +record.get(SOURCE_PORT) + " to " + 
-									record.get(DEST_IP) + ", port " + record.get(DEST_PORT)))
-							.withProperties(new NetworkFlowObject()
-								.withNetworkFlowLabel(new NetworkFlowLabelType()
-									.withSrcSocketAddress(new SocketAddress()
-										.withObjectReference(srcAddressObservable.getId()))
-									.withDestSocketAddress(new SocketAddress()
-										.withObjectReference(dstAddressObservable.getId())))));
-
-					CustomPropertiesType properties = new CustomPropertiesType();
-					if (!record.get(TIMESTAMP_EPOCH_MS).isEmpty())
-						properties
-							.withProperties(new Property() 	
-								.withName("Start_Time")
-								.withValue(record.get(TIMESTAMP_EPOCH_MS)));
-
-					if (!record.get(BYTE_CNT).isEmpty())
-						properties
-							.withProperties(new Property() 	
-								.withName("Total_Bytes")
-								.withValue(record.get(BYTE_CNT)));
-
-					if (!record.get(PACKET_CNT).isEmpty())
-						properties
-							.withProperties(new Property() 	
-								.withName("Total_Packets")
-								.withValue(record.get(PACKET_CNT)));
-
-					if (!properties.getProperties().isEmpty())
-						flowObservable
-							.getObject()
-								.getProperties()
-									.withCustomProperties(properties);
-
-					observables
-						.withObservables(flowObservable);
 				}
 				
 				if (hostnameObservable != null)	{
-					//host -> software relation		
-					RelatedObjectsType relatedObjects = new RelatedObjectsType();
-					if (softwareObservable != null)	
-						relatedObjects			
-							.withRelatedObjects(new RelatedObjectType() //list
-								.withIdref(softwareObservable.getId())
-								.withRelationship(new ControlledVocabularyStringType()
-									.withValue("host runs software"))
-								.withDescription(new StructuredTextType()
-									.withValue(HOSTNAME + " runs " + record.get(PATH)))
-								.withDiscoveryMethod(getMeasureSourceType("Hone")));
 
-					//host -> source address relation
-					if (srcAddressObservable != null)
-						relatedObjects			
-							.withRelatedObjects(new RelatedObjectType() //list
-								.withIdref(srcAddressObservable.getId())
-								.withRelationship(new ControlledVocabularyStringType()
-									.withValue("host uses address"))
-								.withDescription(new StructuredTextType()
-									.withValue(HOSTNAME + " uses address " + record.get(SOURCE_IP) + ", port " + record.get(SOURCE_PORT)))
-								.withDiscoveryMethod(getMeasureSourceType("Hone")));
-					
+					/* host -> software relation */	
+					List<RelatedObjectType> relatedObjects = new ArrayList<RelatedObjectType>();
+					if (softwareObservable != null)	{
+						relatedObjects.add(
+							setRelatedObject(softwareObservable.getId(), "runs", 
+								HOSTNAME + " runs " + record.get(PATH), "Hone"));
+					}
+
+					/* host -> source address relation */
+					if (srcAddressObservable != null) {
+						relatedObjects.add(	
+							setRelatedObject(srcAddressObservable.getId(), "usesAddress", 			
+								HOSTNAME + " uses address " + record.get(SOURCE_IP) + ", port " + record.get(SOURCE_PORT), "Hone"));
+					}
+
 					//if relations exist, adding them to the hostname observable
-					if (!relatedObjects.getRelatedObjects().isEmpty())
+					if (!relatedObjects.isEmpty()) {
 						hostnameObservable
 							.getObject()
-								.withRelatedObjects(relatedObjects);
-					
+								.withRelatedObjects(new RelatedObjectsType()
+									.withRelatedObjects(relatedObjects));
+					}
 					observables
 						.withObservables(hostnameObservable);
 				}
 
 				if (softwareObservable != null)	{
-					//software -> flow relation
-					RelatedObjectsType relatedObjects = new RelatedObjectsType();
-					if (flowObservable != null)
-						relatedObjects
-							.withRelatedObjects(new RelatedObjectType() //list
-								.withIdref(flowObservable.getId())
-								.withRelationship(new ControlledVocabularyStringType()
-									.withValue("software has flow"))
-								.withDescription(new StructuredTextType()
-									.withValue(record.get(PATH) + " has flow " + 
-										record.get(SOURCE_IP) + ", port " + record.get(SOURCE_PORT) +" to " + 
-										record.get(DEST_IP) + ", port " + record.get(DEST_PORT)))
-								.withDiscoveryMethod(getMeasureSourceType("Hone")));
 
-					//software -> account relation
-					if (accountObservable != null)
-						relatedObjects
-							.withRelatedObjects(new RelatedObjectType() //list
-								.withIdref(accountObservable.getId())
-								.withRelationship(new ControlledVocabularyStringType()
-									.withValue("software runs as account"))
-								.withDescription(new StructuredTextType()
-									.withValue(record.get(PATH) + " runs as uid " + record.get(UID) + " on host " + HOSTNAME))
-								.withDiscoveryMethod(getMeasureSourceType("Hone")));
+					/* software -> flow relation */
+					List<RelatedObjectType> relatedObjects = new ArrayList<RelatedObjectType>();
+					if (flowObservable != null) {
+						relatedObjects.add(
+							setRelatedObject(flowObservable.getId(), "hasFlow",
+								record.get(PATH) + " has flow " + record.get(SOURCE_IP) + ", port " + record.get(SOURCE_PORT) +" to " + 
+									record.get(DEST_IP) + ", port " + record.get(DEST_PORT), "Hone"));
+					}
+
+					/* software -> account relation */
+					if (accountObservable != null) {
+						relatedObjects.add(
+							setRelatedObject(accountObservable.getId(), "runsAs",
+								record.get(PATH) + " runs as uid " + record.get(UID) + " on host " + HOSTNAME, "Hone"));	
+					}
 
 					//if relations exist, adding them to the software observable
-					if (!relatedObjects.getRelatedObjects().isEmpty())
+					if (!relatedObjects.isEmpty()) {
 						softwareObservable
 							.getObservableComposition()
 								.getObservables().get(0)
 									.getObject()
-										.withRelatedObjects(relatedObjects);
+										.withRelatedObjects(new RelatedObjectsType()
+											.withRelatedObjects(relatedObjects));
+					}
 					
 					observables
 						.withObservables(softwareObservable);
 				}
 			}
 
-			if (!observables.getObservables().isEmpty())
-				stixPackage = initStixPackage("Hone")
-					.withObservables(observables);
-			else return null;
+			return (observables.getObservables().isEmpty()) ? null : stixPackage.withObservables(observables);
 
 		} catch (DatatypeConfigurationException e)	{
 			e.printStackTrace();
@@ -342,17 +356,6 @@ public class HoneExtractor extends HTMLExtractor	{
 			e.printStackTrace();
 		}
 
-		return stixPackage;
-	}
-	
-	boolean validate(STIXPackage stixPackage) {
-		
-		try	{
-			return stixPackage.validate();
-		}			
-		catch (SAXException e)	{
-			e.printStackTrace();
-		}
-		return false;
+		return null;
 	}
 }
