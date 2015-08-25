803973071d89ba0e83b6f790cbd4951774ec2a45
Maria Vincent <mvincen2@vols.utk.edu>
2015-08-25 17:25:52 -0400

    cleanup

--- src/test/java/HTMLExtractor/HoneExtractorTest.java
+++ src/test/java/HTMLExtractor/HoneExtractorTest.java
@@ -1,20 +1,12 @@
 package STIXExtractor;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
-import java.net.URL;
-import java.nio.charset.Charset;
 
 import org.xml.sax.InputSource;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-
-import org.json.*;
-import org.jsoup.*;
-import org.jsoup.parser.Parser;
 import org.jsoup.Jsoup;
+import org.jsoup.parser.Parser;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -80,12 +72,9 @@ public class HoneExtractorTest	{
 			"49870,6667,4,10.32.92.230,69.42.215.170,2068,2";
 		String hostname = "Mary";
 
-		System.out.println("going into hone");
 		HoneExtractor honeExtractor = new HoneExtractor(honeInfo, hostname);
 		STIXPackage stixPackage = honeExtractor.getStixPackage();
 		
-		System.out.println(stixPackage.toXMLString(true));
-
 		System.out.println("Validating Hone stixPackage");
 		assertTrue(honeExtractor.validate(stixPackage));
 
@@ -93,7 +82,8 @@ public class HoneExtractorTest	{
 
 		System.out.println("Testing 1st element:");
 		Element element = doc.select("cybox|Observable:has(cybox|Title:matches(Host))").first();
-//hostname		
+		
+		//hostname		
 		System.out.println();
 		System.out.println("Testing Hostname:");
 		System.out.println("Testing Id");
@@ -106,21 +96,23 @@ public class HoneExtractorTest	{
 		assertEquals(element.select("cybox|Observable_Source > cyboxCommon|Information_Source_Type").text(), "Hone");
 		System.out.println("Testing Title");
 		assertEquals(element.select("cybox|Title").text(), "Host");
-//hostname -> software relation
+
+		//hostname -> software relation
 		System.out.println("Testing Hostname -> Software relation");
-		String softwareId = element.select("cybox|Related_Object:has(cybox|Relationship:matches(^host runs software$))").attr("idref");
+		String softwareId = element.select("cybox|Related_Object:has(cybox|Relationship:matches(^runs$))").attr("idref");
 		Element software = doc.select("[id=" + softwareId + "]").first();
 		assertEquals(software.select("ProductObj|Product").text(), "/usr/lib/gvfs/gvfsd-smb");
 		System.out.println("Testing Relationship");
-		Element relation = element.select("cybox|Related_Object:has(cybox|Relationship:matches(^host runs software$))").first();
-		assertEquals(relation.select("cybox|Relationship").text(), "host runs software");
+		Element relation = element.select("cybox|Related_Object:has(cybox|Relationship:matches(^runs$))").first();
+		assertEquals(relation.select("cybox|Relationship").text(), "runs");
 		System.out.println("Testing Relation Description");
 		assertEquals(relation.select("cybox|Description").text(), "Mary runs /usr/lib/gvfs/gvfsd-smb");
 		System.out.println("Testing Relation Source");
 		assertEquals(relation.select("cybox|Discovery_Method > cyboxCommon|Information_Source_Type").text(), "Hone");
-//hostname -> address relation
+
+		//hostname -> address relation
 		System.out.println("Testing Hostname -> Address relation");
-		String addressId = element.select("cybox|Related_Object:has(cybox|Relationship:matches(^host uses address$))").attr("idref");
+		String addressId = element.select("cybox|Related_Object:has(cybox|Relationship:matches(^usesAddress$))").attr("idref");
 		Element srcAddress = doc.select("[id=" + addressId + "]").first();
 		String sourceIpId = srcAddress.select("SocketAddressObj|IP_Address").attr("object_reference");
 		String ip = doc.select("[id= " + sourceIpId + "] > cybox|Object > cybox|Properties > AddressObj|Address_Value").text();
@@ -129,13 +121,14 @@ public class HoneExtractorTest	{
 		String port = doc.select("[id= " + sourcePortId + "] > cybox|Object > cybox|Properties > PortObj|Port_Value").text();
 		assertEquals(port, "49870");
 		System.out.println("Testing Relationship");
-		relation = element.select("cybox|Related_Object:has(cybox|Relationship:matches(^host uses address$))").first();
-		assertEquals(relation.select("cybox|Relationship").text(), "host uses address");
+		relation = element.select("cybox|Related_Object:has(cybox|Relationship:matches(^usesAddress$))").first();
+		assertEquals(relation.select("cybox|Relationship").text(), "usesAddress");
 		System.out.println("Testing Relation Description");
 		assertEquals(relation.select("cybox|Description").text(), "Mary uses address 10.32.92.230, port 49870");
 		System.out.println("Testing Relation Source");
 		assertEquals(relation.select("cybox|Discovery_Method > cyboxCommon|Information_Source_Type").text(), "Hone");
-//software 
+
+		//software 
 		element = doc.select("cybox|Observable:has(cybox|Title:matches(Software))").first();
 		System.out.println();
 		System.out.println("Testing Software:");
@@ -158,31 +151,34 @@ public class HoneExtractorTest	{
 		assertEquals(element.select("ProcessObj|Name").text(), "/usr/lib/gvfs/gvfsd-smb");
 		System.out.println("Testing Arguments");
 		assertEquals(element.select("ProcessObj|Argument").text(), "test");
-//software -> flow relation
+
+		//software -> flow relation
 		System.out.println("Testing Software -> Flow relation:");
-		String flowId = element.select("cybox|Related_Object:has(cybox|Relationship:matches(^software has flow$))").attr("idref");
+		String flowId = element.select("cybox|Related_Object:has(cybox|Relationship:matches(^hasFlow$))").attr("idref");
 		Element flow = doc.select("[id=" + flowId + "]").first();
 		assertEquals(flow.select("cybox|Description").text(), "10.32.92.230, port 49870 to 69.42.215.170, port 6667");
 		System.out.println("Testing Relationship");
-		relation = element.select("cybox|Related_Object:has(cybox|Relationship:matches(^software has flow$))").first();
-		assertEquals(relation.select("cybox|Relationship").text(), "software has flow");
+		relation = element.select("cybox|Related_Object:has(cybox|Relationship:matches(^hasFlow$))").first();
+		assertEquals(relation.select("cybox|Relationship").text(), "hasFlow");
 		System.out.println("Testing Relation Description");
 		assertEquals(relation.select("cybox|Description").text(), "/usr/lib/gvfs/gvfsd-smb has flow 10.32.92.230, port 49870 to 69.42.215.170, port 6667");
 		System.out.println("Testing Relation Source");
 		assertEquals(relation.select("cybox|Discovery_Method > cyboxCommon|Information_Source_Type").text(), "Hone");
-//software -> account
+
+		//software -> account
 		System.out.println("Testing Software -> Account relation:");
-		String accountId = element.select("cybox|Related_Object:has(cybox|Relationship:matches(^software runs as account$))").attr("idref");
+		String accountId = element.select("cybox|Related_Object:has(cybox|Relationship:matches(^runsAs$))").attr("idref");
 		Element account = doc.select("[id=" + accountId + "]").first();
 		assertEquals(account.select("cybox|Description").text(), "uid 1000 on host Mary");
 		System.out.println("Testing Relationship");
-		relation = element.select("cybox|Related_Object:has(cybox|Relationship:matches(^software runs as account$))").first();
-		assertEquals(relation.select("cybox|Relationship").text(), "software runs as account");
+		relation = element.select("cybox|Related_Object:has(cybox|Relationship:matches(^runsAs$))").first();
+		assertEquals(relation.select("cybox|Relationship").text(), "runsAs");
 		System.out.println("Testing Relation Description");
 		assertEquals(relation.select("cybox|Description").text(), "/usr/lib/gvfs/gvfsd-smb runs as uid 1000 on host Mary");
 		System.out.println("Testing Relation Source");
 		assertEquals(relation.select("cybox|Discovery_Method > cyboxCommon|Information_Source_Type").text(), "Hone");
-//account
+
+		//account
 		System.out.println();
 		element = doc.select("cybox|Observable:has(cybox|Title:matches(Account))").first();
 		System.out.println("Testing Account:");
@@ -206,7 +202,8 @@ public class HoneExtractorTest	{
 		String destPortId = null;
 
 		for (Element address : elements)	{
-//source address
+
+			//source address
 			if (!address.select("cybox|Description:contains(49870)").text().isEmpty())	{
 				System.out.println();
 				System.out.println("Testing Source Address:");
@@ -216,18 +213,21 @@ public class HoneExtractorTest	{
 				assertEquals(address.select("cyboxCommon|Information_Source_Type").text(), "Hone");
 				System.out.println("Testing Description");
 				assertEquals(address.select("cybox|Description").text(), "10.32.92.230, port 49870");
-//source address -> source IP
+
+				//source address -> source IP
 				System.out.println("Testing Address -> IP relation");
 				sourceIpId = address.select("SocketAddressObj|IP_Address").attr("object_reference");
 				ip = doc.select("[id= " + sourceIpId + "] > cybox|Object > cybox|Properties > AddressObj|Address_Value").text();
 				assertEquals(ip, "10.32.92.230");
-//source address -> source Port
+
+				//source address -> source Port
 				System.out.println("Testing Address -> Port relation");
 				sourcePortId = address.select("SocketAddressObj|Port").attr("object_reference");
 				port = doc.select("[id= " + sourcePortId + "] > cybox|Object > cybox|Properties > PortObj|Port_Value").text();
 				assertEquals(port, "49870");
 			}
-//destination address
+
+			//destination address
 			if (!address.select("cybox|Description:contains(6667)").text().isEmpty())	{
 				System.out.println();
 				System.out.println("Testing Destination Address:");
@@ -237,19 +237,22 @@ public class HoneExtractorTest	{
 				assertEquals(address.select("cyboxCommon|Information_Source_Type").text(), "Hone");
 				System.out.println("Testing Description");
 				assertEquals(address.select("cybox|Description").text(), "69.42.215.170, port 6667");
-//dest address -> dest IP
+
+				//dest address -> dest IP
 				System.out.println("Testing Address -> IP relation");
 				destIpId = address.select("SocketAddressObj|IP_Address").attr("object_reference");
 				ip = doc.select("[id= " + destIpId + "] > cybox|Object > cybox|Properties > AddressObj|Address_Value").text();
 				assertEquals(ip, "69.42.215.170");
-//dest address -> dest Port
+
+				//dest address -> dest Port
 				System.out.println("Testing Address -> Port relation");
 				destPortId = address.select("SocketAddressObj|Port").attr("object_reference");
 				port = doc.select("[id= " + destPortId + "] > cybox|Object > cybox|Properties > PortObj|Port_Value").text();
 				assertEquals(port, "6667");
 			}
 		}
-//source IP
+
+		//source IP
 		System.out.println();
 		System.out.println("Testing Source IP:");
 		if (sourceIpId != null)	{
@@ -267,7 +270,8 @@ public class HoneExtractorTest	{
 			System.out.println("ERROR: Could not verify Source IP or Source Address");
 			assertTrue(false);
 		}
-//destination IP
+
+		//destination IP
 		System.out.println();
 		System.out.println("Testing Destination IP:");
 		if (destIpId != null)	{
@@ -285,7 +289,8 @@ public class HoneExtractorTest	{
 			System.out.println("ERROR: Could not verify Destination IP or Destination Address");
 			assertTrue(false);
 		}
-//source Port
+
+		//source Port
 		System.out.println();
 		System.out.println("Testing Source Port:");
 		if (sourcePortId != null)	{
@@ -303,7 +308,8 @@ public class HoneExtractorTest	{
 			System.out.println("ERROR: Could not verify Source Port or Source Address");
 			assertTrue(false);
 		}
-//destination Port
+
+		//destination Port
 		System.out.println();
 		System.out.println("Testing Destination Port:");
 		if (destPortId != null)	{
@@ -321,7 +327,8 @@ public class HoneExtractorTest	{
 			System.out.println("ERROR: Could not verify Source Port or Source Address");
 			assertTrue(false);
 		}
-//flow
+
+		//flow
 		System.out.println();
 		element = doc.select("cybox|Observable:has(cybox|Title:matches(^Flow$))").first();
 		System.out.println("Testing Flow:");
@@ -330,12 +337,13 @@ public class HoneExtractorTest	{
 		System.out.println("Testing Source");
 		assertEquals(element.select("cyboxCommon|Information_Source_Type").text(), "Hone");
 		System.out.println("Testing ID");
-		assertEquals(element.select("cybox|Object").attr("id"), "stucco:flow-10.32.92.230_49870-69.42.215.170_6667");
+		assertEquals(element.select("cybox|Object").attr("id"), "stucco:flow-169893094_49870-1160435626_6667");
 		String srcSocketId = element.select("NetFlowObj|Src_Socket_Address").attr("object_reference");
 		String destSocketId = element.select("NetFlowObj|Dest_Socket_Address").attr("object_reference");
 		Element srcSocket = doc.select("[id=" + srcSocketId + "]").first();
 		Element destSocket = doc.select("[id=" + destSocketId + "]").first();
-//flow -> source address
+
+		//flow -> source address
 		System.out.println("Testing Flow -> Source Address relation");
 		System.out.println("Testing Flow Source IP");
 		sourceIpId = srcSocket.select("SocketAddressObj|IP_Address").attr("object_reference");
@@ -345,7 +353,8 @@ public class HoneExtractorTest	{
 		sourcePortId = srcSocket.select("SocketAddressObj|Port").attr("object_reference");
 		port = doc.select("[id= " + sourcePortId + "] > cybox|Object > cybox|Properties > PortObj|Port_Value").text();
 		assertEquals(port, "49870");
-//flow -> dest address
+
+		//flow -> dest address
 		System.out.println("Testing Flow -> Dest Address relation");
 		System.out.println("Testing Flow Dest IP");
 		destIpId = destSocket.select("SocketAddressObj|IP_Address").attr("object_reference");
@@ -363,5 +372,28 @@ public class HoneExtractorTest	{
 		assertEquals(element.select("cyboxCommon|Property[name=Total_Packets]").text(), "2");
 		System.out.println("Testing Total Bytes");
 		assertEquals(element.select("cyboxCommon|Property[name=Total_Bytes]").text(), "2068");
+	}
+	
+	/**
+	 * Test two elements
+	 */
+	@Test
+	public void test_two_elements()	{
+
+		System.out.println("STIXExtractor.HoneExtractorTest.test_one_element_with_header()");
+		String honeInfo = 
+			"user,uid,proc_pid,proc_ppid,path,argv,conn_id,timestamp_epoch_ms," +
+			"source_port,dest_port,ip_version,source_ip,dest_ip,byte_cnt,packet_cnt\n" + 
+			"someUser,1000,3144,3140,/usr/lib/gvfs/gvfsd-smb,test,10000,1371797596390," +
+			"49870,6667,4,10.32.92.230,69.42.215.170,2068,2\n" +
+			"someUser,1000,3144,3140,/usr/lib/gvfs/gvfsd-smb,test,10000,1371797596390," +
+			"49870,6667,4,10.32.92.230,69.42.215.170,2068,2";
+		String hostname = "Mary";
+
+		HoneExtractor honeExtractor = new HoneExtractor(honeInfo, hostname);
+		STIXPackage stixPackage = honeExtractor.getStixPackage();
+		
+		System.out.println("Validating Hone stixPackage");
+		assertTrue(honeExtractor.validate(stixPackage));
 	}
 }
