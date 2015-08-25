39576aa3fa50f05777f2620cd40245ec354d6c08
Maria Vincent <mvincen2@vols.utk.edu>
2015-08-25 13:35:59 -0400

    cleanup

--- src/main/java/HTMLExtractor/LoginEventExtractor.java
+++ src/main/java/HTMLExtractor/LoginEventExtractor.java
@@ -2,45 +2,24 @@ package STIXExtractor;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
-import java.util.UUID;
 
-import java.io.Reader;
-import java.io.StringReader;
 import java.io.IOException;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.xml.datatype.XMLGregorianCalendar;
-import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.namespace.QName;					
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.mitre.stix.stix_1.STIXPackage;
-import org.mitre.stix.stix_1.STIXHeaderType;
 import org.mitre.cybox.cybox_2.Observables;
 import org.mitre.cybox.cybox_2.Observable;
-import org.mitre.cybox.cybox_2.ObjectType;
 import org.mitre.cybox.cybox_2.RelatedObjectType;
 import org.mitre.cybox.cybox_2.RelatedObjectsType;
-import org.mitre.cybox.common_2.Property;
-import org.mitre.cybox.common_2.ControlledVocabularyStringType;
-import org.mitre.cybox.common_2.StringObjectPropertyType;
-import org.mitre.cybox.common_2.StructuredTextType;
 import org.mitre.cybox.common_2.DateTimeObjectPropertyType;
-import org.mitre.cybox.objects.Hostname;
-import org.mitre.cybox.objects.Product;
-import org.mitre.cybox.objects.AccountObjectType;
-import org.mitre.cybox.objects.UserAccountObjectType;
-import org.mitre.cybox.objects.CategoryTypeEnum;
+import org.mitre.cybox.common_2.ControlledVocabularyStringType;
 import org.mitre.cybox.objects.UserSession;
 
 import org.xml.sax.SAXException;			
@@ -69,38 +48,28 @@ public class LoginEventExtractor extends HTMLExtractor	{
 	private STIXPackage extract (String loginEventInfo)	{
 
 		try	{
-
-			CSVFormat csvFormat = CSVFormat.DEFAULT.withHeader(HEADERS);
-			Reader reader = new StringReader(loginEventInfo);
-			CSVParser csvParser = new CSVParser(reader, csvFormat);
-			List<CSVRecord> records = csvParser.getRecords();
+			stixPackage = initStixPackage("LoginEvent");				
+			Observables observables = initObservables();
+			List<CSVRecord> records = getCSVRecordsList(HEADERS, loginEventInfo);
 			
-			if (records.size() == 0) return null;
-			
+			if (records.isEmpty()) {
+				return null;
+			}
+ 
 			int start;
 			CSVRecord record = records.get(0);
 			if (record.get(0).equals(DATE_TIME))	{
-				if (record.size() == 1) return null;
-				else start = 1;
+				if (record.size() == 1) {
+					return null;
+				} else {
+					start = 1;
+				}
 			}
 			else start = 0;
 
-			GregorianCalendar calendar = new GregorianCalendar();
-			XMLGregorianCalendar now = DatatypeFactory.newInstance().newXMLGregorianCalendar(				
-				new GregorianCalendar(TimeZone.getTimeZone("UTC")));
-			stixPackage = new STIXPackage()				
- 				.withSTIXHeader(new STIXHeaderType().
-					withTitle("LoginEvent")) 
-				.withTimestamp(now)
-	 			.withId(new QName("gov.ornl.stucco", "LoginEvent-" + UUID.randomUUID().toString(), "stucco"));
-			Observables observables = new Observables()
-				.withCyboxMajorVersion("2.0")
-				.withCyboxMinorVersion("1.0");
-
 		 	for (int i = start; i < records.size(); i++)	{
-			
+				
 				record = records.get(i);
-				System.out.println(record);
 
 				Observable hostnameObservable = null;
 				Observable accountObservable = null;
@@ -108,86 +77,60 @@ public class LoginEventExtractor extends HTMLExtractor	{
 				Observable hostAtIpObservable = null;
 				Observable ipObservable = null;
 				
-				QName hostnameId = new QName("gov.ornl.stucco", "hostname-" + UUID.randomUUID().toString(), "stucco");
-				QName accountId = new QName("gov.ornl.stucco", "account-" + UUID.randomUUID().toString(), "stucco");
-				QName softwareId = new QName("gov.ornl.stucco", "software-" + UUID.randomUUID().toString(), "stucco");
-				QName hostAtIpId = new QName("gov.ornl.stucco", "hostname-" + UUID.randomUUID().toString(), "stucco");
-				QName ipId = new QName("gov.ornl.stucco", "ip-" + UUID.randomUUID().toString(), "stucco");
-
-				//hostname
+				/* hostname */
 				if (!record.get(HOSTNAME).isEmpty())	{
-					hostnameObservable = new Observable()	
-						.withId(hostnameId)
-						.withTitle("Hostname")
-						.withObservableSources(getMeasureSourceType("LoginEvent"))
-						.withObject(new ObjectType()
-							.withId(new QName("gov.ornl.stucco", "hostname-" + record.get(HOSTNAME), "stucco"))
-							.withDescription(new StructuredTextType()
-								.withValue(record.get(HOSTNAME)))
-							.withProperties(new Hostname()
-								.withHostnameValue(new StringObjectPropertyType()
-									.withValue(record.get(HOSTNAME)))));
-			
-					if (!record.get(LOGIN_SOFTWARE).isEmpty())
-						hostnameObservable
-							.getObject()
-								.withRelatedObjects(new RelatedObjectsType()
-									.withRelatedObjects(new RelatedObjectType()
-										.withIdref(softwareId)
-										.withDescription(new StructuredTextType()
-											.withValue(record.get(HOSTNAME) + " runs " + record.get(LOGIN_SOFTWARE)))
-										.withRelationship(new ControlledVocabularyStringType()
-											.withValue("runs"))));
-				
-					observables
-						.withObservables(hostnameObservable);
+					hostnameObservable = setHostObservable(record.get(HOSTNAME), "LoginEvent");
+				}
+
+				/* account */
+				if (!record.get(USER).isEmpty())	{
+					accountObservable = setAccountObservable(record.get(USER), "LoginEvent");
 				}
 				
+				/* software */
+				if (!record.get(LOGIN_SOFTWARE).isEmpty())	{
+					softwareObservable = setSoftwareObservable(record.get(LOGIN_SOFTWARE), "LoginEvent");
+					observables
+						.withObservables(softwareObservable);
+				}
 
-				//account
-				if (!record.get(USER).isEmpty())	{
-					accountObservable = new Observable()	
-						.withId(accountId)
-						.withTitle("Account")
-						.withObservableSources(getMeasureSourceType("LoginEvent"))
-						.withObject(new ObjectType()
-							.withId(new QName("gov.ornl.stucco", "account-" + record.get(USER), "stucco"))
-							.withDescription(new StructuredTextType()
-								.withValue(record.get(USER)))
-							.withProperties(new UserAccountObjectType()
-								.withUsername(new StringObjectPropertyType()
-									.withValue(record.get(USER)))
-								.withDescription(new StringObjectPropertyType()
-									.withValue(record.get(USER)))));
+				/* IP */
+				if (!record.get(FROM_IP).isEmpty())	{
+					ipObservable = setIpObservable(record.get(FROM_IP), "LoginEvent");
+					observables
+						.withObservables(ipObservable);
+				}
 
+				/* host */
+				if (!record.get(FROM_IP).isEmpty())	{
+					hostAtIpObservable = setHostObservable("host_at_" + record.get(FROM_IP), "LoginEvent");	
+				}
+
+				if (accountObservable != null) {
 					List<RelatedObjectType> relatedObjects = new ArrayList<RelatedObjectType>();
-					if (!record.get(HOSTNAME).isEmpty())
+	
+					/* account -> hostname relation */
+					if (hostnameObservable != null) {
 						relatedObjects.add(
-							new RelatedObjectType()
-								.withIdref(hostnameId)
-								.withDescription(new StructuredTextType()
-									.withValue(record.get(USER) + " logs in to " + record.get(HOSTNAME))) 
-								.withRelationship(new ControlledVocabularyStringType()
-									.withValue("logsInTo"))
+							setRelatedObject(hostnameObservable.getId(), "logsInTo", record.get(USER) + " logs in to " + record.get(HOSTNAME), "LoginEvent")  
 								.withState(new ControlledVocabularyStringType()
 									.withValue(record.get(STATUS)))
 								.withProperties(new UserSession()
 									.withLoginTime(new DateTimeObjectPropertyType()
 										.withValue(record.get(DATE_TIME)))));
-					if (!record.get(FROM_IP).isEmpty())
+					}
+					
+					/* account -> ip relation */
+					if (ipObservable != null) {
 						relatedObjects.add(
-							new RelatedObjectType()
-								.withIdref(hostAtIpId)
-								.withDescription(new StructuredTextType()
-									.withValue(record.get(USER) + " logs in from host at " + record.get(FROM_IP))) 
-								.withRelationship(new ControlledVocabularyStringType()
-									.withValue("logsInFrom"))
+							setRelatedObject(hostAtIpObservable.getId(), "logsInFrom", record.get(USER) + " logs in from host at " + record.get(FROM_IP), "LoginEvent") 
 								.withState(new ControlledVocabularyStringType()
 									.withValue(record.get(STATUS)))
 								.withProperties(new UserSession()
 									.withLoginTime(new DateTimeObjectPropertyType()
 										.withValue(record.get(DATE_TIME)))));
-					if (!relatedObjects.isEmpty())
+					}
+					
 					accountObservable
 						.getObject()
 							.withRelatedObjects(new RelatedObjectsType()
@@ -196,72 +139,33 @@ public class LoginEventExtractor extends HTMLExtractor	{
 						.withObservables(accountObservable);
 				}
 
-				//software
-				if (!record.get(LOGIN_SOFTWARE).isEmpty())	{
-					softwareObservable = new Observable()	
-						.withId(softwareId)
-						.withTitle("Software")
-						.withObservableSources(getMeasureSourceType("LoginEvent"))
-						.withObject(new ObjectType()
-							.withId(new QName("gov.ornl.stucco", "software-" + record.get(LOGIN_SOFTWARE), "stucco"))
-							.withDescription(new StructuredTextType()
-								.withValue(record.get(LOGIN_SOFTWARE)))
-							.withProperties(new Product()
-								.withProduct(new StringObjectPropertyType()
-									.withValue(record.get(LOGIN_SOFTWARE)))));
+				/* host -> software relation */
+				if (hostnameObservable != null) {
+					if (softwareObservable != null) {
+						hostnameObservable
+							.getObject()
+								.withRelatedObjects(new RelatedObjectsType()
+									.withRelatedObjects(setRelatedObject(softwareObservable.getId(), "runs", record.get(HOSTNAME) + " runs " + record.get(LOGIN_SOFTWARE), "LoginEvent")));
+					}
 					observables
-						.withObservables(softwareObservable);
+						.withObservables(hostnameObservable);
 				}
-				
-				//ip		
-				if (!record.get(FROM_IP).isEmpty())	{
-					ipObservable = new Observable()
-						.withId(ipId)
-						.withTitle("IP")
-						.withObservableSources(getMeasureSourceType("LoginEvent"))
-						.withObject(new ObjectType()
-							.withId(new QName("gov.ornl.stucco", "ip-" + record.get(FROM_IP), "stucco"))
-							.withDescription(new org.mitre.cybox.common_2.StructuredTextType()
-								.withValue(record.get(FROM_IP))) 
-							.withProperties(setAddress(record.get(FROM_IP), CategoryTypeEnum.IPV_4_ADDR)));
-					observables
-						.withObservables(ipObservable);		
-				}
-				//host
-				if (!record.get(FROM_IP).isEmpty())	{
-					hostAtIpObservable = new Observable()	
-						.withId(hostAtIpId)
-						.withTitle("Hostname")
-						.withObservableSources(getMeasureSourceType("LoginEvent"))
-						.withObject(new ObjectType()
-							.withId(new QName("gov.ornl.stucco", "hostname-" + "host_at_" + record.get(FROM_IP), "stucco"))
-							.withDescription(new StructuredTextType()
-								.withValue("host at " + record.get(FROM_IP)))
-							.withProperties(new Hostname()
-								.withHostnameValue(new StringObjectPropertyType()
-									.withValue("host_at_" + record.get(FROM_IP)))));
-					if (!record.get(FROM_IP).isEmpty())
+
+				/* hostAtIp -> ip */
+				if (hostAtIpObservable != null) {
+					if (ipObservable != null) {
 						hostAtIpObservable
 							.getObject()
 								.withRelatedObjects(new RelatedObjectsType()
-									.withRelatedObjects(new RelatedObjectType()
-										.withIdref(ipId)
-										.withDescription(new StructuredTextType()
-											.withValue("host at " + record.get(FROM_IP) + " has IP " + record.get(FROM_IP))) 
-										.withRelationship(new ControlledVocabularyStringType()
-											.withValue("hasIP"))
-										.withState(new ControlledVocabularyStringType()
-											.withValue(record.get(STATUS)))
-										.withProperties(new UserSession()
-											.withLoginTime(new DateTimeObjectPropertyType()
-													.withValue(record.get(DATE_TIME))))));
+									.withRelatedObjects(
+										setRelatedObject(ipObservable.getId(), "hasIP", "host_at_" + record.get(FROM_IP) + " has IP " + record.get(FROM_IP), "LoginEvent")));
+					}
 					observables
-						.withObservables(hostAtIpObservable);		
+						.withObservables(hostAtIpObservable);
 				}
 			}
-			stixPackage
-				.withObservables(observables);
-			System.out.println(stixPackage.toXMLString(true));
+
+			return (observables.getObservables().isEmpty()) ? null : stixPackage.withObservables(observables);
 
 		} catch (DatatypeConfigurationException e)	{
 			e.printStackTrace();
@@ -270,17 +174,6 @@ public class LoginEventExtractor extends HTMLExtractor	{
 			e.printStackTrace();
 		}
 
-		return stixPackage;
-	}
-	
-	boolean validate(STIXPackage stixPackage) {
-		
-		try	{
-			return stixPackage.validate();
-		}			
-		catch (SAXException e)	{
-			e.printStackTrace();
-		}
-		return false;
+		return null;
 	}
 }
--- src/test/java/HTMLExtractor/LoginEventExtractorTest.java
+++ src/test/java/HTMLExtractor/LoginEventExtractorTest.java
@@ -20,10 +20,10 @@ import STIXExtractor.LoginEventExtractor;
 public class LoginEventExtractorTest	{
 	
 	/**
-	 * Test empty document
+	 * Test empty document no header
 	 */
 	@Test
-	public void test_empty_document()	{
+	public void test_empty_document_no_header()	{
 
 		System.out.println("STIXExtractor.LoginEventExtractorTest.test_empty_document()");
 
@@ -31,7 +31,25 @@ public class LoginEventExtractorTest	{
 
 		LoginEventExtractor loginEventExtractor = new LoginEventExtractor(loginEventInfo);
 		STIXPackage stixPackage = loginEventExtractor.getStixPackage();
+		
+		System.out.println("Testing that package is null");
+		assertTrue(stixPackage == null);
+	}
+	
+	/**
+	 * Test empty document with header
+	 */
+	@Test
+	public void test_empty_document_with_header()	{
 
+		System.out.println("STIXExtractor.LoginEventExtractorTest.test_empty_document()");
+
+		String loginEventInfo = 
+			"date_time,hostname,login_software,status,user,from_ip";
+
+		LoginEventExtractor loginEventExtractor = new LoginEventExtractor(loginEventInfo);
+		STIXPackage stixPackage = loginEventExtractor.getStixPackage();
+		
 		System.out.println("Testing that package is null");
 		assertTrue(stixPackage == null);
 	}
@@ -50,21 +68,22 @@ public class LoginEventExtractorTest	{
 		LoginEventExtractor loginEventExtractor = new LoginEventExtractor(loginEventInfo);
 		STIXPackage stixPackage = loginEventExtractor.getStixPackage();
 
-		System.out.println("Validating EventLogin stixPackage");
+		System.out.println("Validating LoginEvent stixPackage");
 		loginEventExtractor.validate(stixPackage);
 
 		Document doc = Jsoup.parse(stixPackage.toXMLString(), "", Parser.xmlParser());
 		Element element = doc.select("cybox|Observable:has(HostnameObj|Hostname_Value:matches(^StuccoHost\\Z))").first();
 
+		System.out.println();
 		System.out.println("Testing Hostname:");
 		System.out.println("Testing Name");
 		assertEquals(element.select("HostnameObj|Hostname_Value").text(), "StuccoHost");
 		System.out.println("Testing Description");
 		assertEquals(element.select("cybox|Object > cybox|Description").text(), "StuccoHost");
 		System.out.println("Testing Source");
-		assertEquals(element.select("cyboxCommon|Information_Source_Type").text(), "LoginEvent");
+		assertEquals(element.select("cybox|Observable_Source > cyboxCommon|Information_Source_Type").text(), "LoginEvent");
 		System.out.println("Testing Title");
-		assertEquals(element.select("cybox|Title").text(), "Hostname");
+		assertEquals(element.select("cybox|Title").text(), "Host");
 		System.out.println("Testing Hostname -> Software relation");
 		String softwareId = element.select("cybox|Related_Object:has(cybox|Relationship:matches(^runs\\Z))").attr("idref");
 		Element software = doc.select("[id=" + softwareId + "]").first();
@@ -72,15 +91,16 @@ public class LoginEventExtractorTest	{
 		
 		element = doc.select("cybox|Observable:has(HostnameObj|Hostname_Value:matches(^host_at_192.168.10.11\\Z))").first();
 
+		System.out.println();
 		System.out.println("Testing Hostname:");
 		System.out.println("Testing Name");
 		assertEquals(element.select("HostnameObj|Hostname_Value").text(), "host_at_192.168.10.11");
 		System.out.println("Testing Description");
-		assertEquals(element.select("cybox|Object > cybox|Description").text(), "host at 192.168.10.11");
+		assertEquals(element.select("cybox|Object > cybox|Description").text(), "host_at_192.168.10.11");
 		System.out.println("Testing Source");
-		assertEquals(element.select("cyboxCommon|Information_Source_Type").text(), "LoginEvent");
+		assertEquals(element.select("cybox|Observable_Source > cyboxCommon|Information_Source_Type").text(), "LoginEvent");
 		System.out.println("Testing Title");
-		assertEquals(element.select("cybox|Title").text(), "Hostname");
+		assertEquals(element.select("cybox|Title").text(), "Host");
 		System.out.println("Testing Hostname_at_IP -> IP relation");
 		String ipId = element.select("cybox|Related_Object:has(cybox|Relationship:matches(^hasIP\\Z))").attr("idref");
 		Element ip = doc.select("[id=" + ipId + "]").first();
@@ -88,13 +108,14 @@ public class LoginEventExtractorTest	{
 		
 		element = doc.select("cybox|Observable:has(cybox|Title:matches(^Account\\Z))").first();
 
+		System.out.println();
 		System.out.println("Testing Account:");
 		System.out.println("Testing Name");
 		assertEquals(element.select("UserAccountObj|Username").text(), "StuccoUser");
 		System.out.println("Testing Description");
 		assertEquals(element.select("AccountObj|Description").text(), "StuccoUser");
 		System.out.println("Testing Source");
-		assertEquals(element.select("cyboxCommon|Information_Source_Type").text(), "LoginEvent");
+		assertEquals(element.select("cybox|Observable_Source > cyboxCommon|Information_Source_Type").text(), "LoginEvent");
 		System.out.println("Testing Title");
 		assertEquals(element.select("cybox|Title").text(), "Account");
 		System.out.println("Testing Account -> Hostname relation");
@@ -108,6 +129,7 @@ public class LoginEventExtractorTest	{
 		
 		element = doc.select("cybox|Observable:has(cybox|Title:matches(^Software\\Z))").first();
 
+		System.out.println();
 		System.out.println("Testing Software:");
 		System.out.println("Testing Name");
 		assertEquals(element.select("ProductObj|Product").text(), "sshd");
@@ -120,6 +142,7 @@ public class LoginEventExtractorTest	{
 		
 		element = doc.select("cybox|Observable:has(cybox|Title:matches(^IP\\Z))").first();
 
+		System.out.println();
 		System.out.println("Testing IP:");
 		System.out.println("Testing Name");
 		assertEquals(element.select("AddressObj|Address_Value").text(), "192.168.10.11");
